3db4fee79d18f909c19c627bf1a29db28473f04a
yukhanov <s.yukhanov@gmail.com>
2022-03-21 16:39:33 +0300

    first commit

---
+++ Tools for interface/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Tools for interface
+//
+//  Created by Сергей Юханов on 25.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+}
+
---
+++ Tools for interface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tools for interface
+//
+//  Created by Сергей Юханов on 22.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
