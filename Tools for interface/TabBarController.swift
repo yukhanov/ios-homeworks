e9c413bc5ff509d76500eabf28311d71d3f3324f
yukhanov <s.yukhanov@gmail.com>
2022-04-15 14:31:09 +0300

    for merge

---
+++ Tools for interface/TabBarController.swift
@@ -0,0 +1,58 @@
+//
+//  TabBarController.swift
+//  Tools for interface
+//
+//  Created by Сергей Юханов on 22.03.2022.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    enum TabBarItem {
+        case feed
+        case profile
+        
+        var title: String {
+            switch self {
+            case .feed:
+                return "Лента"
+            case .profile:
+                return "Профиль"
+            }
+        }
+        var image: UIImage? {
+            switch self {
+            case .feed:
+                return UIImage(systemName: "house")
+            case .profile:
+                return UIImage(systemName: "person.circle")
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTabBar()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setupTabBar() {
+           let items: [TabBarItem] = [.feed, .profile]
+           
+           self.viewControllers = items.map({ tabBarItem in
+               switch tabBarItem {
+               case .feed:
+                   return UINavigationController(rootViewController: ViewController())
+               case .profile:
+                   return SecondViewController()
+               }
+           })
+           self.viewControllers?.enumerated().forEach({ (index, vc) in
+               vc.tabBarItem.title = items[index].title
+               vc.tabBarItem.image = items[index].image
+           })
+       }
+}
